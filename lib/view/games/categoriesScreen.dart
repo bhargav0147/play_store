046460ee314ae072d6fae94590fdc435e32a3099
R76 <darshansakhat166@gmail.com>
2023-05-04 15:30:59 +0530

    05/05/2023

--- lib/view/games/categoriesScreen.dart
+++ lib/view/games/categoriesScreen.dart
@@ -10,6 +10,43 @@ class CategoriesScreen extends StatefulWidget {
 class _CategoriesScreenState extends State<CategoriesScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Categories"));
+    return SafeArea(child: Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+        child: ListView(
+          children: [
+            cate("Action"),
+            cate("Advanture"),
+            cate("Arcade"),
+            cate("Board"),
+            cate("Card"),
+            cate("Casino"),
+            cate("Casual"),
+            cate("Education"),
+            cate("Music"),
+            cate("Puzzle"),
+            cate("Racing"),
+            cate("Role Playing"),
+            cate("Simulation"),
+            cate("Sports"),
+            cate("Stategy"),
+            cate("Trivia"),
+            cate("Word"),
+          ],
+        )
+        ),
+      ),
+    );
+  }
+  Widget cate(String name)
+  {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5),
+      height: 40,
+      width: double.infinity,
+      child: Text(
+        "$name",style: TextStyle(letterSpacing: 1,fontSize: 20,color: Colors.black87,fontFamily: 'robo'),
+      ),
+    );
   }
 }
